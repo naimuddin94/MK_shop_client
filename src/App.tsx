b9c265995c973ec1b678f84c96ab16008a922e9a
naimuddin94 <developernaimuddin@gmail.com>
2024-07-10 22:32:25 +0600

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="font-gumela">
+      <h1>This is App component</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
